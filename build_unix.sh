41e4cda2debca888ab869e7c033153b804445633
Paweł Kołodziejski <aquadran@gmail.com>
2022-05-15 08:38:26 +0200

    update to 5.15.4

--- build_unix.sh
+++ build_unix.sh
@@ -3,7 +3,7 @@
 #
 # MIT License
 #
-# Copyright (c) 2021 Pawel Kolodziejski
+# Copyright (c) 2021-2022 Pawel Kolodziejski
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -23,7 +23,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-QT_VERSION=5.15.2
+QT_VERSION=5.15.4
 QT_VERSION_BASE=`echo $QT_VERSION | cut -d'.' -f 1,2`
 
 NUM_THREADS=1
@@ -54,8 +54,8 @@ case `uname -s` in
 	;;
 esac
 
-if [ ! -f "qt-everywhere-src-$QT_VERSION.tar.xz" ]; then
-	wget https://download.qt.io/archive/qt/$QT_VERSION_BASE/$QT_VERSION/single/qt-everywhere-src-$QT_VERSION.tar.xz
+if [ ! -f "qt-everywhere-opensource-src-$QT_VERSION.tar.xz" ]; then
+	wget https://download.qt.io/archive/qt/$QT_VERSION_BASE/$QT_VERSION/single/qt-everywhere-opensource-src-$QT_VERSION.tar.xz
 	if [ $? != 0 ]; then
 		echo "ERROR: Failed to download Qt sources!"
 		exit 1
@@ -63,11 +63,14 @@ if [ ! -f "qt-everywhere-src-$QT_VERSION.tar.xz" ]; then
 fi
 
 if [ ! -d "qt-everywhere-src-$QT_VERSION" ]; then
-	tar xf qt-everywhere-src-$QT_VERSION.tar.xz
+	tar xf qt-everywhere-opensource-src-$QT_VERSION.tar.xz
 	if [ $? != 0 ]; then
 		echo "ERROR: Failed to unpack Qt archive!"
 		exit 1
 	fi
+	cd qt-everywhere-src-$QT_VERSION/qtbase
+	cat ../../patches/qiosurfacegraphicsbuffer.patch | patch -p1
+	cd ../..
 fi
 
 pushd qt-everywhere-src-$QT_VERSION
